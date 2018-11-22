4e840c0b3f5692c9aa69255e5b1061ae99d4a619
Premshay <premshay@gmail.com>
2018-11-22 14:42:44 +0200

    Loging Elevate aim - not working (s04e147)

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -1,15 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "BattleTank.h"
 #include "TankBarrel.h"
+#include "BattleTank.h"
 
 
-void UTankBarrel::Elevate(float DegreesPerSecond)
+void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	// move barrel the right amount this frame
-
 	// given a max elevation speed and the frame time
-
-	UE_LOG(LogTemp, Warning, TEXT("Barrel-Elevate() called at speed %f"), DegreesPerSecond);
-
 }
--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -20,7 +20,6 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Setup)
 	void SetBarrelReference(UTankBarrel* BarrelToSet);
 
-	
 protected:
 	UTankAimingComponent* TankAimingComponent = nullptr;
 
@@ -28,20 +27,13 @@ private:
 	// Sets default values for this pawn's properties
 	ATank();
 
-	UPROPERTY(EditAnywhere, Category = Firing)
-		float LaunchSpeed = 100000; // TOTO find sensible default
-
-protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	
+	UPROPERTY(EditAnywhere, Category = Firing)
+	float LaunchSpeed = 100000; // TOTO find sensible default
 	
 };
